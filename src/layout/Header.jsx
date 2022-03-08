6816f60808af75354e6fdb9a5bc2d05184eadde1
Alexander Vasiliev <asvasi25@gmail.com>
2022-03-08 10:03:05 +0300

    Add preliminary styling with Materialize

---
+++ src/layout/Header.jsx
@@ -0,0 +1,18 @@
+const Header = () => {
+  return (
+    <nav className="blue darken-4">
+      <div className="nav-wrapper">
+        <a href="!#" className="brand-logo left">
+          React Movies
+        </a>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li>
+            <a href="!#">GitHub</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
---
+++ src/layout/Main.jsx
@@ -0,0 +1,5 @@
+const Main = () => {
+  return <main className="container content"> Movie App</main>;
+};
+
+export default Main;
