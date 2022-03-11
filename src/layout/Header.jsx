4836b60a1d6cd70f2e25a81f38072fe3ebff96a3
Alexander Vasiliev <asvasi25@gmail.com>
2022-03-11 10:07:13 +0300

    Fix links

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,12 +2,12 @@ const Header = () => {
   return (
     <nav className="blue darken-4">
       <div className="nav-wrapper">
-        <a href="!#" className="brand-logo left">
+        <a href="../../public/index.html" className="brand-logo left">
           React Movies
         </a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li>
-            <a href="!#">GitHub</a>
+            <a href="https://github.com/sshvasi/react-movies">GitHub</a>
           </li>
         </ul>
       </div>
